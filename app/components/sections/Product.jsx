dbb08e749529fd9544b2cf31ff8156cb533aab0e
José Cañizares <jose.canizares@colorado.edu>
2017-05-10 15:57:53 -0600

    fix padding on footer text and subtitle text

--- app/components/sections/Product.jsx
+++ app/components/sections/Product.jsx
@@ -7,16 +7,18 @@ class Product extends Component {
         <div className='header-text'>
           <h2>Expert knowledge, motivation, and support in your pocket.</h2>
           </div>
-            <div className='header-text'>
-          <h4><strong>The key to our success is building things people need and love to use. Warriorfy takes all our popular resources and put's them into one powerful platform to repair one's relationship to food, body, and self.</strong></h4>
-        </div>
+          <div className='header-text'>
+            <h4><strong>The key to our success is building things people need and love to use. Warriorfy takes all our popular resources and put's them into one powerful platform to repair one's relationship to food, body, and self.</strong></h4>
+          </div>
 
         <div className='product'>
             <img src={require('../img/Product-1.png')} className={'product-image'}/>
                   </div>
 
         <iframe width="560" height="315" src="https://www.youtube.com/embed/FzO-U60-85M" frameborder="0" allowfullscreen></iframe>
-<p>Our content address the great challenges of life: work, relationships, friendship, anxiety and more.</p>
+          <div className='footer-text'>
+            <p>Our content address the great challenges of life: work, relationships, friendship, anxiety and more.</p>
+          </div>
       </div>
     )
   }
