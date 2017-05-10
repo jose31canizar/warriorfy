c3e68a2ac9ae62a2388aa6e624a2268a4073378c
José Cañizares <jose.canizares@colorado.edu>
2017-05-10 15:19:04 -0600

    refix opportunity sass for problems and solutions

--- app/components/sections/Opportunity.jsx
+++ app/components/sections/Opportunity.jsx
@@ -8,78 +8,87 @@ class Opportunity extends Component {
           <h2>Worldwide, 2.3 billion people are overweight or obese and rates of eating disorders are skyrocketing.</h2>
         </div>
         <div className='content'>
-          <div className='problems'>
+          <div className='top-labels'>
             <div className='problems-label'>
               <h4>PROBLEM</h4>
-            </div>
-            <div className='problems-label-subtitle'>
               <h6><strong>Diets are...</strong></h6>
             </div>
-            <div className='problem-block'>
-              <p> <strong>Ineffective in the long-term</strong></p>
-              <p>People often are confused.</p>
+            <div className='solutions-label'>
+              <h4>SOLUTION</h4>
+              <h6><strong>Warriorfy provides...</strong></h6>
             </div>
-            <div className='problem-block'>
-              <p><strong>Increase anxiety</strong></p>
-              <p>People are often distracted.</p>
+          </div>
+
+          <div className='block-row-container'>
+            <div className='block-row'>
+              <div className='block'>
+                <p><strong>Ineffective in the long-term</strong></p>
+                <p>People often are confused.</p>
+              </div>
+              <div className='block'>
+                <img src={require("../img/03-arrows-3.png")}/>
+              </div>
+              <div className='block'>
+                <p><strong>Insight</strong></p>
+                <p>People get clarity.</p>
+              </div>
             </div>
-            <div className='problem-block'>
-              <p><strong>Lower body confidence</strong></p>
-              <p>Reinforces the concept of not being good enough</p>
+
+            <div className='block-row'>
+              <div className='block'>
+                <p><strong>Increase anxiety</strong></p>
+                <p>People are often distracted.</p>
+              </div>
+              <div className='block'>
+                <img src={require("../img/03-arrows-3.png")}/>
+              </div>
+              <div className='block'>
+              <p><strong>Guidance</strong></p>
+                <p>People get focus.</p>
+              </div>
             </div>
-            <div className='problem-block'>
+          </div>
+
+
+          <div className='block-row'>
+            <div className='block'>
               <p><strong>Lower body confidence</strong></p>
               <p>Increases shame, isolation, anxiety and depression</p>
             </div>
-          </div>
-
-          <div className='problems arrows'>
-            <div className='problems-label'>
-              <h4 className='arrows-label'>PROBLEM</h4>
+            <div className='block'>
+              <img src={require("../img/03-arrows-3.png")}/>
             </div>
-            <div className='problems-label-subtitle'>
-              <h6 className='arrows-label'><strong>Diets are...</strong></h6>
+            <div className='block'>
+              <p><strong>Support</strong></p>
+              <p>Connects people to certified coaches who provide nutritional and emotional support</p>
             </div>
-            <img className='problem-block arrow' src={require("../img/03-arrows-3.png")}/>
-            <img className='problem-block arrow' src={require("../img/03-arrows-3.png")}/>
-            <img className='problem-block arrow' src={require("../img/03-arrows-3.png")}/>
-            <img className='problem-block arrow' src={require("../img/03-arrows-3.png")}/>
           </div>
 
 
-
-
-          <div className={'solutions'}>
-            <div className={'solutions-label'}>
-              <h4>SOLUTION</h4>
+          <div className='block-row'>
+            <div className='block'>
+              <p><strong>Lower body confidence</strong></p>
+              <p>Increases shame, isolation, anxiety and depression</p>
             </div>
-            <div className={'solutions-label-subtitle'}>
-              <h6><strong>Warriorfy provides...</strong></h6>
+            <div className='block'>
+              <img src={require("../img/03-arrows-3.png")}/>
             </div>
-            <div className={'solution-block'}>
-              <p><strong>Insight</strong></p>
-              <p>People get clarity.</p>
-            </div>
-            <div className={'solution-block'}>
-            <p><strong>Guidance</strong></p>
-              <p>People get focus.</p>
-            </div>
-            <div className={'solution-block'}>
+            <div className='block'>
               <p><strong>Support</strong></p>
               <p>Connects people to certified coaches who provide nutritional and emotional support</p>
             </div>
           </div>
         </div>
 
-          <div className={'overall'}>
-            <div className={'problem'}>
-              <div className={'problem-label'}>
+          <div className='overall'>
+            <div className='problem'>
+              <div className='problem-label'>
                 <h4>Overall Problem</h4>
                   <p>Dieting is ineffective at long term weight management, yet continues to be prescribed by the media and medical doctors.</p>
               </div>
             </div>
-            <div className={'solution'}>
-              <div className={'solution-label'}>
+            <div className='solution'>
+              <div className='solution-label'>
                 <h4>Solution</h4>
                 <p>Build an immersive educational experience that connects people to expert knowledge, health data, and a coach.</p>
               </div>
