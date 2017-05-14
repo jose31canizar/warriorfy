52c1282d622ce3a745c3039e3982bf15dd7bc5ba
Jessica Raymond <Jessica@Jessicas-MacBook-Air-2.local>
2017-05-13 21:32:48 -0600

    trying to get the mockup images to align in three separate columns and style the text. No luck.

--- app/components/sections/Product.jsx
+++ app/components/sections/Product.jsx
@@ -11,19 +11,31 @@ class Product extends Component {
         </div>
           <div className='product-series'>
               <div className='image-1'>
+                <div className='title-text'>
+                  <h6>Audio Content</h6>
+                </div>
+                <div className='subtitle-text'>
+                  <p>Find inspiration through education and personal stories of success</p>
+                </div>
                 <img src={require('../img/product-image-2.png')} className={'product-image'}/>
-                <h6>Audio inspiration and education</h6>
-                <img src={require('../img/icon-audio.png')} className={'product-image'}/>
               </div>
               <div className='image-2'>
+                <div className='title-text'>
+                  <h6>Easy Tracking</h6>
+                </div>
+                <div className='subtitle-text'>
+                <p>Conveniently log meals and bring self-awareness to emotions, behaviors, and thoughts.</p>
+                </div>
                 <img src={require('../img/product-image-3.png')} className={'product-image'}/>
-                <h6>Easy tracking of meals and mood</h6>
-                <img src={require('../img/icon-tracking.png')} className={'product-image'}/>
               </div>
               <div className='image-3'>
+               <div className='title-text'>
+                 <h6>Personal Coach</h6>
+               </div>
+              <div className='subtitle-text'>
+                <p>Increase accountability and reduce shame with 1:1 chat-based support</p>
+              </div>
                 <img src={require('../img/product-image-4.png')} className={'product-image'}/>
-                <h6>Personal chat-based support</h6>
-                <img src={require('../img/icon-chat.png')} className={'product-image'}/>
               </div>
           </div>
         <div className='product'>
