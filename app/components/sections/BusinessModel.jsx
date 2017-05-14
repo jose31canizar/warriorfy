6597787b51a15975d2c55a07957eafd62b9f3f37
Jessica Raymond <Jessica@Jessicas-MacBook-Air-2.local>
2017-05-13 22:20:07 -0600

    updated text in business model page.

--- app/components/sections/BusinessModel.jsx
+++ app/components/sections/BusinessModel.jsx
@@ -8,7 +8,7 @@ class BusinessModel extends Component {
         </div>
         <div className='subtitle-text'>
           <h3>1.) Freemium model with 10% conversion from free to paid features</h3>
-          <h3>2.) 5% churn equates to a LTV of $X and allows us to spend up to $Y to acquire a free user</h3>
+          <h3>2.) 30% churn equates to a LTV of $315 and allows us to spend up to $15 to acquire a free user</h3>
           <h3>3.) At 1M users our projected revenue is $9.5M</h3>
         </div>
       </div>
