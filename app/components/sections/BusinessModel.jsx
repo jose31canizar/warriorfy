edcc34b6eb6a549630841e052a2eb33f1f3a25be
Jessica Raymond <Jessica@Jessicas-MacBook-Air-2.local>
2017-05-14 12:15:16 -0600

    styling changes to user growth title

--- app/components/sections/BusinessModel.jsx
+++ app/components/sections/BusinessModel.jsx
@@ -7,12 +7,13 @@ class BusinessModel extends Component {
         <div className='header'>
         </div>
         <div className='subtitle-text'>
-          <h3>1.) Freemium model with 10% conversion from free to paid features</h3>
-          <h3>2.) 30% churn equates to a LTV of $315 and allows us to spend up to $15 to acquire a free user</h3>
-          <h3>3.) At 1M users our projected revenue is $9.5M</h3>
+          <h3>1.) Freemium model with 10% conversion from free to paid plan</h3>
+          <h3>2.) At 1M users our projected MRR is $1.3M </h3>
         </div>
       <div className='.product-image'>
-      <h6>User Growth</h6>
+         <div className='growth-title'>
+          <h6>User Growth</h6>
+         </div>
       <img src={require('../img/user-growth-projected.png')} />
       </div>
       </div>
