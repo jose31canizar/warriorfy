08d7e6f6337cd908a980c409693b50553a86e36f
Nolawee Mengist <nome9478@colorado.edu>
2017-05-25 20:48:47 -0600

    centered problems and solutions just need to edit labels for mobile

--- app/components/sections/Opportunity.jsx
+++ app/components/sections/Opportunity.jsx
@@ -6,7 +6,38 @@ class Opportunity extends Component {
     return (
       <div className='container'>
         <Label label='Problems'/>
-        <Label label='Solutions'/>
+        <h6><strong>People who struggle with food can be...</strong></h6>
+        <div className='block-row'>
+          <div className='block'>
+            <p><strong>Misinformed</strong></p>
+            <p>Research shows restrictive diets are ineffective at long-term weight management.</p>
+          </div>
+          <div className='block'>
+            <p><strong>Anxious and Depressed</strong></p>
+            <p>Many people turn towards food or away from food in an effort to cope with mood disorders.</p>
+          </div>
+          <div className='block'>
+            <p><strong>Inadequately Supported</strong></p>
+              <p>Failed attempts to control food and weight lead to guilt, shame, disappointment, and isolation.</p>
+          </div>
+        </div>
+
+      <Label label='Solutions'/>
+        <div className='block-row'>
+          <div className='block'>
+            <p><strong>Education</strong></p>
+            <p>Our subject matter experts share valuable knowledge on how to make lifestyle changes that last.</p>
+          </div>
+          <div className='block'>
+            <p><strong>Coping Strategies</strong></p>
+            <p>We provide evidence-based strategies to help people manage and improve their anxiety and depression.</p>
+          </div>
+          <div className='block'>
+            <p><strong>Support</strong></p>
+            <p>We connect people to certified coaches who provide nutritional and emotional chat-based support.</p>
+          </div>
+        </div>
+
         <div className={'header'}>
           <h2>Worldwide, 2.3 billion people are overweight or obese and rates of eating disorders are skyrocketing.</h2>
         </div>
